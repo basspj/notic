746da159775e7ed875fe1409f0042ad0b8dc8ee7
basspj <thanapol.pj@gmail.com>
2017-11-03 12:48:28 +0700

    chore: add storybook

---
+++ storybook/stories/CenterView/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+import style from './style';
+
+export interface ICenterViewProps {
+  children: any;
+}
+
+const CenterView: React.SFC<ICenterViewProps> = ({
+  children,
+}: ICenterViewProps) => <View style={style.main}>{children}</View>;
+
+CenterView.defaultProps = {
+  children: null,
+};
+
+export default CenterView;
