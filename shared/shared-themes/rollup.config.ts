f2e85fb719e93dc6a5789d8c0271af9498e8b40e
basspj <thanapol.pj@gmail.com>
2018-01-16 21:59:24 +0700

    chore: update rollup
    affects: @shared/themes

--- shared/shared-themes/rollup.config.ts
+++ shared/shared-themes/rollup.config.ts
@@ -1,35 +1,45 @@
-import { camelCase } from 'lodash';
 import alias from 'rollup-plugin-alias';
 import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve';
 import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+import ts from 'typescript';
+
 import path from 'path';
+import { camelCase } from 'lodash';
 
-import packageJson from './package.json';
+import pkg from './package.json';
 
-const { name } = packageJson;
+const libraryName = 'shared-themes';
+const outDir = 'src';
 const index = 'index';
-const outDir = 'build';
-
-// CommonJS (for Node) and ES module (for bundlers) build.
-// (We could have three entries in the configuration array
-// instead of two, but it's quicker to generate multiple
-// builds from a single configuration where possible, using
-// the `targets` option which can specify `dest` and `format`)
+const outputOptions = {
+  sourcemap: true,
+};
 
 export default {
-  // entry: `${outDir}/${libraryName}.js`,
-  entry: `${outDir}/${index}.js`,
-  // Indicate here external modules you don't
-  // wanna include in your bundle (i.e.: 'lodash')
+  input: `${outDir}/${index}.ts`,
+  output: [
+    { file: pkg.main, name: camelCase(libraryName), format: 'umd', ...outputOptions },
+    { file: pkg.module, format: 'es', ...outputOptions },
+  ],
+  watch: {
+    include: `${outDir}/**`,
+  },
   plugins: [
+    // Compile TypeScript files
+    typescript({
+      typescript: ts,
+      useTsconfigDeclarationDir: true,
+      clean: true,
+    }),
     // Allow bundling cjs modules
     // (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    nodeResolve(),
+    resolve(),
 
     // Resolve source maps to the original source
     sourceMaps(),
@@ -38,12 +48,4 @@ export default {
       resolve: ['.js', '.jsx', '/index.js'],
     }),
   ],
-  sourcemap: true,
-  targets: [
-    { dest: packageJson.main, moduleName: camelCase(name), format: 'umd' },
-    { dest: packageJson.module, format: 'es' },
-  ],
-  watch: {
-    include: `${outDir}/**`,
-  },
 };
